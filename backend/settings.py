2d6158eec6aad02e254eccc6403a6bd3cf5087fc
Hung Truong <hungtruongquoc@gmail.com>
2024-07-12 10:51:29 -0500

    Fixed the Procfile again

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['.elasticbeanstalk.com', '127.0.0.1']
 
 
 # Application definition
