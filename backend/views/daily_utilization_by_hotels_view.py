69d8ec980c54575d2ba4e528c37b46170b049cdc
Hung Truong <hungtruongquoc@gmail.com>
2024-07-12 22:52:02 -0500

    Fixed the wrong method

--- backend/views/daily_utilization_by_hotels_view.py
+++ backend/views/daily_utilization_by_hotels_view.py
@@ -8,7 +8,7 @@ class DailyUtilizationByHotelsView(APIView):
 
     def get(self, request, month=None):
         try:
-            data = RoomUtilizationService.get_monthly_utilization_by_hotels(month)
+            data = RoomUtilizationService.get_utilization_by_day(month)
             return Response(data, status=status.HTTP_200_OK)
         except ValueError as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
